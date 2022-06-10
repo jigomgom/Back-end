c5108a0a025b347a7b20c204784474d896c50e1b
eank0108 <eank0108@naver.com>
2022-06-10 17:32:38 +0900

    User Table name 설정

--- src/main/java/com/mini/babmeokeon/controller/UserController.java
+++ src/main/java/com/mini/babmeokeon/controller/UserController.java
@@ -1,4 +1,11 @@
 package com.mini.babmeokeon.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    @PostMapping("/api/signup")
+    public
 }
--- src/main/java/com/mini/babmeokeon/model/User.java
+++ src/main/java/com/mini/babmeokeon/model/User.java
@@ -1,7 +1,24 @@
 package com.mini.babmeokeon.model;
 
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
 
 @Table(name ="users")
+@Entity
 public class User {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    @JsonIgnore
+    private String password;
+
 }
