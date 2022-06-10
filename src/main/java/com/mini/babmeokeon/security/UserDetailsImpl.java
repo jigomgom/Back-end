9688b1498155b8cfaabca2c99236fd5c080e07b0
jeonghyeonuk <junghunwook456@naver.com>
2022-06-10 19:55:58 +0900

    로그인 기능 생성

---
+++ src/main/java/com/mini/babmeokeon/security/UserDetailsImpl.java
@@ -0,0 +1,57 @@
+package com.mini.babmeokeon.security;
+
+import com.mini.babmeokeon.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<? extends GrantedAuthority>  authorities = new ArrayList<>();
+        return authorities;
+    }
+}
---
+++ src/main/java/com/mini/babmeokeon/security/provider/JWTAuthProvider.java
@@ -0,0 +1,41 @@
+package com.mini.babmeokeon.security.provider;
+
+
+import com.mini.babmeokeon.model.User;
+import com.mini.babmeokeon.repository.UserRepository;
+import com.mini.babmeokeon.security.UserDetailsImpl;
+import com.mini.babmeokeon.security.jwt.JwtDecoder;
+import com.mini.babmeokeon.security.jwt.JwtPreProcessingToken;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JWTAuthProvider implements AuthenticationProvider {
+
+    private final JwtDecoder jwtDecoder;
+
+    private final UserRepository userRepository;
+
+    @Override
+    public Authentication authenticate(Authentication authentication)
+            throws AuthenticationException {
+        String token = (String) authentication.getPrincipal();
+        String username = jwtDecoder.decodeUsername(token);
+
+
+        User user = userRepository.findByUsername(username);
+
+        UserDetailsImpl userDetails = new UserDetailsImpl(user);
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return JwtPreProcessingToken.class.isAssignableFrom(authentication);
+    }
+}
