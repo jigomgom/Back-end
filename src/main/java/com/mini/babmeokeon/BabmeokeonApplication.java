2da4f4ac35ecfb350e1fd4b0901e0bd0d90b4bfb
minsu <>
2022-06-10 17:16:18 +0900

    초기 프로젝트 파일 생성

---
+++ src/main/java/com/mini/babmeokeon/BabmeokeonApplication.java
@@ -0,0 +1,13 @@
+package com.mini.babmeokeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabmeokeonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabmeokeonApplication.class, args);
+    }
+
+}
